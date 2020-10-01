7d10cd09124f7bddb69bd82105abdbd9d2b5959a
parth45 <72157914+parth45@users.noreply.github.com>
2020-10-01 12:49:51 +0530

    Update hello.py
    i added new question and its answer is it useful for beginner

--- Day01-15/Day01/hello.py
+++ Day01-15/Day01/hello.py
@@ -17,3 +17,22 @@ print('hello, world!')
 print('你好', '世界')
 print('hello', 'world', sep=', ', end='!')
 print('goodbye, world', end='!\n')
+
+
+#mWrite a program which accepts a sequence of comma-separated numbers
+# from console and generate a list and a tuple which contains every number.
+# Suppose the following input is supplied to the program
+
+l=[]
+t=()
+for i in range(5):
+	my_num=int(input(print("hey type ur number i, will convert it into list and tuple")))
+	l.append(my_num)
+print(tuple(l))
+
+
+
+print(l)
+print()
+
+
